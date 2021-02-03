331dddbf6f22f62095b62e0fa1530abc6949ba70
Fatih <fatihealan@gmail.com>
2021-02-02 19:37:36 -0500

    third commit

--- src/test/java/Test/fatih/userStory42.java
+++ src/test/java/Test/fatih/userStory42.java
@@ -4,5 +4,6 @@ public class userStory42 {
     public static void main(String[] args) {
         System.out.println("Hello there");
         System.out.println("Something else");
+        System.out.println("third thing in a row");
     }
 }
