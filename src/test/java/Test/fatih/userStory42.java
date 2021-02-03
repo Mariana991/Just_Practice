15d12b5a1a13653ce3a0a672c97c404e02ffb0fe
Fatih <fatihealan@gmail.com>
2021-02-02 19:34:35 -0500

    second commit

--- src/test/java/Test/fatih/userStory42.java
+++ src/test/java/Test/fatih/userStory42.java
@@ -3,5 +3,6 @@ package Test.fatih;
 public class userStory42 {
     public static void main(String[] args) {
         System.out.println("Hello there");
+        System.out.println("Something else");
     }
 }
